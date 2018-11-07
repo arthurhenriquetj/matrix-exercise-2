9e865724a6f218a3a88e187b2c668904aec28b67
arthurhenriquetj <arthurhenriquetj@gmail.com>
2018-11-07 17:29:59 -0200

    completando a base

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,15 +9,20 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Digit the number of lines:");
 		int n1 = sc.nextInt(); // recebendo o numero de linhas da matriz
+		System.out.println("Digit the number of columns:");
 		int n2 = sc.nextInt(); // recebendo o numero de colunas da matriz
 
 		int[][] mat = new int[n1][n2]; // matriz instanciada na memoria
 
 		for (int i = 0; i < n1; i++)
 			for (int j = 0; j < n2; j++) {
+				System.out.println("Digit the number of position  " + i + " , " + j);
 				mat[i][j] = sc.nextInt();
 			} // preenchendo os campos da matriz
+		
+		
 
 		sc.close();
 	}
